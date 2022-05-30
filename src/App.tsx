9379a3db612fb511ce29dcf0727c45315f66c4b3
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-05-30 16:56:29 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>this is the best SPACE APP</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>HOLA!!!</h1>
       </header>
     </div>
   );
