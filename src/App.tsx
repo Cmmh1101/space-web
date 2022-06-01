a8a446ee5b0ec834b0168948f40165b83ce21dec
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-06-01 15:04:50 +0100

    Styles setup

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>this is the best SPACE APP</p>
-        <h1>HOLA OTRA VEZ!!!</h1>
-      
+        <h5 className="heading-5">SO, YOU WANT TO TRAVEL TO</h5>
+        <h1 className="heading heading-1">SPACE</h1>
+        <div className="subheading subheading-2">AVG. DISTANCE</div>
+        <div className="heading subheading-1">384,400 KM</div>
+        <div className="subheading nav-text">EUROPA</div>
+        <p>Lorem ipsum dolor</p>
       </header>
     </div>
   );
