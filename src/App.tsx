c202ac22b497dd0018f645ae3094b9ff3bd3ed8c
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-06-01 12:35:02 +0100

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>this is the best SPACE APP</p>
-      <h1>HOLA OTRA VEZ!!!</h1>
+        <h1>HOLA OTRA VEZ!!!</h1>
+      
       </header>
     </div>
   );
