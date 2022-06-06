f120187695d1bb83a8386ea4d623bd120e92d705
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-06-06 18:32:07 +0100

    headerbox component setup

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,22 +1,20 @@
 import { Link } from 'react-router-dom';
 import { Button } from 'reactstrap';
+import HeaderBox from './HeaderBox';
 
 const Hero = () => {
   return (
     <div className='hero px-5'>
       <div className='row overlay p-10 mx-lg-5'>
-        <article className='col-11 col-lg-6 hero-text'>
-          <div className='heading-5 text-uppercase'>
-            So, you want to travel to
-          </div>
-          <div className='heading-1 text-uppercase'>space</div>
-          <p>
-            Let's face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we'll give you a truly out of this
-            world experience!
-          </p>
-        </article>
+        <HeaderBox
+          head5='So, you want to travel to'
+          head1='space'
+          content="Let's face it; if you want to go to space, you might as well genuinely
+        go to outer space and not hover kind of on the edge of it. Well sit
+        back, and relax because we'll give you a truly out of this world
+        experience!"
+        />
+
         <Button className='btn hero-btn text-uppercase mt-5'>
           <Link to='/destination'>Explore</Link>
         </Button>
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,9 +2,9 @@ import Hero from '../components/Hero';
 
 const Home = () => {
   return (
-    <div className='hero'>
+    <>
       <Hero />
-    </div>
+    </>
   );
 };
 export default Home;
