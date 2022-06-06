f2f2138f1e3d6b7e6a660462404553b8d045587e
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-06-06 12:30:55 +0100

    hero text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,20 @@
 const Hero = () => {
   return (
     <div className='d-flex'>
-      <div className='container text-content'>
-        <div className='heading-5'>SO, YOU WANT TO TRAVEL TO SPACE</div>
-        <div className='heading-1'>SPACE</div>
-        <p>
-          Let’s face it; if you want to go to space, you might as well genuinely
-          go to outer space and not hover kind of on the edge of it. Well sit
-          back, and relax because we’ll give you a truly out of this world
-          experience!
-        </p>
+      <div className='container '>
+        <div className='row '>
+          <div className='col col-lg-5 text-content'>
+            <div className='heading-5'>SO, YOU WANT TO TRAVEL TO</div>
+            <div className='heading-1'>SPACE</div>
+            <p className='text-wrap'>
+              Let's face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we'll give you a truly out of
+              this world experience!
+            </p>
+          </div>
+          <div className='col'></div>
+        </div>
       </div>
       <div className='btn-content'></div>
     </div>
