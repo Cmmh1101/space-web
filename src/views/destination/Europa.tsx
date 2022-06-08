a80230378688b62d18cadb48944f10a19381a18f
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-06-08 12:36:23 +0100

    destination views complete

--- src/views/destination/Europa.tsx
+++ src/views/destination/Europa.tsx
@@ -6,33 +6,34 @@ import Stats from '../../components/Stats';
 const Destination = () => {
   return (
     <div className='destination'>
-      <div className='sub-header'>
+      <div className='col col-lg-6 sub-header'>
         <SubHeader
           num={'01'}
           text={'Pick your destination'}
           image={'../../assets/destination/image-europa.png'}
         />
       </div>
-
-      <div className='secondary-navbar'>
-        <SecondaryNavbar url='/moon' destination='Moon' />
-        <SecondaryNavbar url='/mars' destination='Mars' />
-        <SecondaryNavbar url='/europa' destination='Europa' />
-        <SecondaryNavbar url='/titan' destination='Titan' />
-      </div>
-      <div className='header-box'>
-        <HeaderBox
-          head3={'Europa'}
-          // customClass={'heading-3'}
-          content={
-            'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.'
-          }
-        />
-      </div>
-      <div className='line'></div>
-      <div className='distance'>
-        <Stats subhead2={'Avg. Distance'} figures={'628 mil. km'} />
-        <Stats subhead2={'Est. Travel Time'} figures={'3 years'} />
+      <div className='col col-lg-6 destination-content'>
+        <div className='secondary-navbar'>
+          <SecondaryNavbar url='/moon' destination='Moon' />
+          <SecondaryNavbar url='/mars' destination='Mars' />
+          <SecondaryNavbar url='/europa' destination='Europa' />
+          <SecondaryNavbar url='/titan' destination='Titan' />
+        </div>
+        <div className='header-box'>
+          <HeaderBox
+            head3={'Europa'}
+            // customClass={'heading-3'}
+            content={
+              'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.'
+            }
+          />
+        </div>
+        <div className='line'></div>
+        <div className='distance'>
+          <Stats subhead2={'Avg. Distance'} figures={'628 mil. km'} />
+          <Stats subhead2={'Est. Travel Time'} figures={'3 years'} />
+        </div>
       </div>
     </div>
   );
--- src/views/destination/Titan.tsx
+++ src/views/destination/Titan.tsx
@@ -6,33 +6,34 @@ import Stats from '../../components/Stats';
 const Destination = () => {
   return (
     <div className='destination'>
-      <div className='sub-header'>
+      <div className='col col-lg-6 sub-header'>
         <SubHeader
           num={'01'}
           text={'Pick your destination'}
           image={'../../assets/destination/image-titan.png'}
         />
       </div>
-
-      <div className='secondary-navbar'>
-        <SecondaryNavbar url='/moon' destination='Moon' />
-        <SecondaryNavbar url='/mars' destination='Mars' />
-        <SecondaryNavbar url='/europa' destination='Europa' />
-        <SecondaryNavbar url='/titan' destination='Titan' />
-      </div>
-      <div className='header-box'>
-        <HeaderBox
-          head3={'Titan'}
-          // customClass={'heading-3'}
-          content={
-            'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.'
-          }
-        />
-      </div>
-      <div className='line'></div>
-      <div className='distance'>
-        <Stats subhead2={'Avg. Distance'} figures={'1.6 bil. km'} />
-        <Stats subhead2={'Est. Travel Time'} figures={'7 years'} />
+      <div className='col col-lg-6 destination-content'>
+        <div className='secondary-navbar'>
+          <SecondaryNavbar url='/moon' destination='Moon' />
+          <SecondaryNavbar url='/mars' destination='Mars' />
+          <SecondaryNavbar url='/europa' destination='Europa' />
+          <SecondaryNavbar url='/titan' destination='Titan' />
+        </div>
+        <div className='header-box'>
+          <HeaderBox
+            head3={'Titan'}
+            // customClass={'heading-3'}
+            content={
+              'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.'
+            }
+          />
+        </div>
+        <div className='line'></div>
+        <div className='distance'>
+          <Stats subhead2={'Avg. Distance'} figures={'1.6 bil. km'} />
+          <Stats subhead2={'Est. Travel Time'} figures={'7 years'} />
+        </div>
       </div>
     </div>
   );
