42ada6841215bb722062748cb826e207f02136b8
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-06-10 17:36:48 +0100

    technology mobile complete

--- src/views/Technology.tsx
+++ src/views/Technology.tsx
@@ -28,7 +28,7 @@ const Technology = () => {
               subhead4='The Terminology...'
               subhead5={'Launch Vehicle'}
               content={
-                "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
+                "  A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
               }
             />
           </div>
