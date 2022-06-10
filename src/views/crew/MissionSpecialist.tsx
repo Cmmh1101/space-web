a5eb1223ecbdebdf60127eefaed4831a6bd4050c
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-06-10 10:04:50 +0100

    crew mobile margins update

--- src/views/crew/MissionSpecialist.tsx
+++ src/views/crew/MissionSpecialist.tsx
@@ -7,27 +7,29 @@ const Crew = () => {
     <div className='crew'>
       <div className='col col-lg-6 sub-header '>
         <SubHeader num={'02'} text={'Meet your crew'} />
+      </div>
+      <div className='crew-body'>
         <div className='image'>
           <img src='../../assets/crew/image-mark-shuttleworth.webp' />
           <div className='line'></div>
         </div>
-      </div>
-      <div className='col col-lg-6 crew-content'>
-        <div className='dot-navbar'>
-          <DotNavbar url='/commander' />
-          <DotNavbar url='/flightEngineer' />
-          <DotNavbar url='/missionSpecialist' />
-          <DotNavbar url='/pilot' />
-        </div>
+        <div className='col col-lg-6 crew-content'>
+          <div className='dot-navbar'>
+            <DotNavbar url='/commander' />
+            <DotNavbar url='/flightEngineer' />
+            <DotNavbar url='/missionSpecialist' />
+            <DotNavbar url='/pilot' />
+          </div>
 
-        <div className='header-box'>
-          <HeaderBox
-            subhead4='Mission Specialist'
-            subhead5={'Mark Shuttleworth'}
-            content={
-              ' Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.'
-            }
-          />
+          <div className='header-box'>
+            <HeaderBox
+              subhead4='Mission Specialist'
+              subhead5={'Mark Shuttleworth'}
+              content={
+                ' Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.'
+              }
+            />
+          </div>
         </div>
       </div>
     </div>
